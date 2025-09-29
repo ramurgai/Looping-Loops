f398e71d722f7971b6abe83f53e5910a249464de
ramurgai <ramurgai@my.waketech.edu>
2025-09-29 09:11:15 -0400

    Updated Do While Program

--- ChoicesChoicesChoices.java
+++ ChoicesChoicesChoices.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
-
+import java.time.LocalTime;
 
 public class ChoicesChoicesChoices {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int userChoice = 0;
-    do{
-        System.out.print("Choose one of the options, 1: Print a random number 1-10, 2: do nothing, 3: exit:  ");
-        userChoice = input.nextInt();
-        switch (userChoice){
-            case 1:
-                System.out.println((int) ((Math.random() * 10) + 1));
-                break;
-            case 2:
-                System.out.println("");
-                break;
-            case 3:
-                System.out.println("You have successfully exited.");
-                break;
-        }
-    } while(userChoice != 3);
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int userChoice = 0;
+        do{
+            System.out.print("Choose one of the options, 1: Say hello, 2: display current local time, 3: exit: ");
+            userChoice = input.nextInt();
+            switch (userChoice){
+                case 1:
+                    System.out.println("Hello!");
+                    break;
+                case 2:
+                    System.out.println(LocalTime.now());
+                    break;
+                case 3:
+                    System.out.println("You have successfully exited.");
+                    break;
+            }
+        } while(userChoice != 3);
+    }
 }
